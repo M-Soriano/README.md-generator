4bdeb12d198ddf76da62062d709b724335bc05e2
Miguel Soriano <originalsorianom@gmail.com>
2022-07-03 18:52:06 -0700

    Added code to capture user input

---
+++ app.js
@@ -0,0 +1,20 @@
+
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+
+  
+const printProfileData = profileDataArr => {
+    // This...
+    //for (let i = 0; i < profileDataArr.length; i += 1) {
+      //console.log(profileDataArr[i]);
+    //}
+
+    //console.log('===========');
+
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+  };
+  
+
+  
+  printProfileData(profileDataArgs);git 
