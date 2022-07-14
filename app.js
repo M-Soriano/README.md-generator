fbdcf3a716bdecd1ab28e9f181ed3459367868d8
Miguel Soriano <originalsorianom@gmail.com>
2022-07-14 04:11:11 -0700

    Exporting functions to new directory dist and cleaning code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
 
+const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
 
 
@@ -134,20 +134,20 @@ if (!portfolioData.projects) {
 
 
 promptUser()
-.then(promptProject)
-.then(portfolioData => {
-  return generatePage(portfolioData);
-})
-.then(pageHTML => {
-  return writeFile(pageHTML);
-})
-.then(writeFileResponse => {
-  console.log(writeFileResponse);
-  return copyFile();
-})
-.then(copyFileResponse => {
-  console.log(copyFileResponse);
-})
-.catch(err => {
-  console.log(err);
-});
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
