82e5b5c1cf122d5187a0c22eafa0c1410e41f2cf
Miguel Soriano <originalsorianom@gmail.com>
2022-07-15 00:39:47 -0700

    updated files placement

---
+++ index.js
@@ -0,0 +1,56 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs');
+const filemaker = require("./filemaker");
+/*// TODO: Create an array of questions for user input
+const questions = [
+    
+    {
+    type: 'input',
+    message: 'What is the name of the project?',
+    name: 'title',
+    default: true
+},
+{
+    type: 'input',
+    message: 'Enter a description of your project:',
+    name: 'descripton',
+    default: true
+},
+{
+    type: 'input',
+    message: 'Enter name of Author:',
+    name: 'Author',
+    default: true
+},
+{
+    type: 'input',
+    message: 'Enter Email informaiton:',
+    name: 'Email',
+    default: true
+},
+{
+    type: 'input',
+    message: 'Do you want to include a website:',
+    name: 'website',
+    default: true
+},
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName,data);
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then((an))
+}
+
+// Function call to initialize app
+init();
+*/
+
+console.log(inquirer);
